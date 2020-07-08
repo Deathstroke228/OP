6b24d63e30123d6731b9232e912262eb05d9102c
Deathstroke228 <61599965+Deathstroke228@users.noreply.github.com>
2020-07-08 07:36:35 +0300

    Add files via upload

---
+++ 24/24.1.pas
@@ -0,0 +1,50 @@
+PROGRAM TreeSort(INPUT, OUTPUT);
+TYPE 
+  Tree = ^NodeType;
+  NodeType = RECORD
+               Ch: CHAR;
+               LLink, RLink: Tree
+             END;
+VAR
+  Root: Tree;
+  Ch: CHAR;
+PROCEDURE Insert(VAR Ptr: Tree; Data: CHAR);
+BEGIN {Insert}
+  IF Ptr = NIL
+  THEN
+    BEGIN {Создаем лист со значением Data}
+      NEW(Ptr);
+      Ptr^.LLink := NIL;
+      Ptr^.RLink := NIL;
+      Ptr^.Ch := Data
+    END
+  ELSE
+    BEGIN
+      IF Ptr^.Ch > Data
+      THEN
+        Insert(Ptr^.LLink, Data)
+      ELSE
+        Insert(Ptr^.RLink, Data)
+    END
+END;  {Insert}
+PROCEDURE PrintTree(Ptr: Tree);
+BEGIN {PrintTree}
+  IF Ptr <> NIL
+  THEN  {Печатает поддерево слева, вершину, поддерево справа}
+    BEGIN
+      PrintTree(Ptr^.LLink);
+      WRITE(Ch);
+      PrintTree(Ptr^.RLink)
+    END;
+  WRITELN
+END;  {PrintTree}
+BEGIN {TreeSort}
+  Root := NIL;
+  WHILE NOT EOLN
+  DO
+    BEGIN
+      READ(Ch);
+      Insert(Root, Ch)
+    END;
+  PrintTree(Root);
+END.  {TreeSort}
