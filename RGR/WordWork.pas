d739af3fe4827eb7c0a125c57f8a81c6206d0822
Deathstroke228 <61599965+Deathstroke228@users.noreply.github.com>
2020-07-09 12:55:45 +0300

    Add files via upload

--- RGR/WordWork.pas
+++ RGR/WordWork.pas
@@ -41,71 +41,12 @@ IMPLEMENTATION
   FUNCTION RegisterChange(Ch: CHAR): CHAR;
   BEGIN {RegisterChange}
     RegisterChange := Ch;
-    CASE Ch OF
-      'À': RegisterChange := 'à';
-      'Á': RegisterChange := 'á';
-      'Â': RegisterChange := 'â';
-      'Ã': RegisterChange := 'ã';
-      'Ä': RegisterChange := 'ä';
-      'Å': RegisterChange := 'å';
-      'Æ': RegisterChange := 'æ';
-      'Ç': RegisterChange := 'ç';
-      'È': RegisterChange := 'è';
-      'É': RegisterChange := 'é';
-      'Ê': RegisterChange := 'ê';
-      'Ë': RegisterChange := 'ë';
-      'Ì': RegisterChange := 'ì';
-      'Í': RegisterChange := 'í';
-      'Î': RegisterChange := 'î';
-      'Ï': RegisterChange := 'ï';
-      'Ð': RegisterChange := 'ð';
-      'Ñ': RegisterChange := 'ñ';
-      'Ò': RegisterChange := 'ò';
-      'Ó': RegisterChange := 'ó';
-      'Ô': RegisterChange := 'ô';
-      'Õ': RegisterChange := 'õ';
-      'Ö': RegisterChange := 'ö';
-      '×': RegisterChange := '÷';
-      'Ø': RegisterChange := 'ø';
-      'Ù': RegisterChange := 'ù';
-      'Ú': RegisterChange := 'ú';
-      'Û': RegisterChange := 'û';
-      'Ü': RegisterChange := 'ü';
-      'Ý': RegisterChange := 'ý';
-      'Þ': RegisterChange := 'þ';
-      'ß': RegisterChange := 'ÿ'
-    END;
-    IF ((Ch = '¨') OR (Ch = '¸'))
+    IF Ch IN ['À'..'ß', 'A'..'Z']
     THEN
-      RegisterChange := 'å';  
-    CASE Ch OF
-      'A': RegisterChange := 'a';
-      'B': RegisterChange := 'b';
-      'C': RegisterChange := 'c';
-      'D': RegisterChange := 'd';
-      'E': RegisterChange := 'e';
-      'F': RegisterChange := 'f';
-      'G': RegisterChange := 'g';
-      'H': RegisterChange := 'h';
-      'I': RegisterChange := 'i';
-      'J': RegisterChange := 'j';
-      'K': RegisterChange := 'k';
-      'L': RegisterChange := 'l';
-      'M': RegisterChange := 'm';
-      'N': RegisterChange := 'n';
-      'O': RegisterChange := 'o';
-      'P': RegisterChange := 'p';
-      'Q': RegisterChange := 'q';
-      'R': RegisterChange := 'r';
-      'S': RegisterChange := 's';
-      'T': RegisterChange := 't';
-      'U': RegisterChange := 'u';
-      'V': RegisterChange := 'v';
-      'W': RegisterChange := 'w';
-      'X': RegisterChange := 'x';
-      'Y': RegisterChange := 'y';
-      'Z': RegisterChange := 'z'
-    END
+      RegisterChange := CHR(ORD(Ch) + 32);
+    IF Ch IN ['¨', '¸']
+    THEN
+      RegisterChange := 'å'
 END;  {RegisterChange}
 
 BEGIN
